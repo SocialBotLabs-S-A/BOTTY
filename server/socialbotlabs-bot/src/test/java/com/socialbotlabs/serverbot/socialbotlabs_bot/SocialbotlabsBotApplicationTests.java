3bf72e3a33f01739d50deed62f793111be94a654
sebah <sebascursoshtml@gmail.com>
2025-02-06 19:00:12 -0300

    Feature/SBO-2: Project Configuration(Ecosystems, Package).

---
+++ server/socialbotlabs-bot/src/test/java/com/socialbotlabs/serverbot/socialbotlabs_bot/SocialbotlabsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialbotlabs.serverbot.socialbotlabs_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialbotlabsBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
