76b0c4fd2ca4512b49a948f8f4a4b29dd36aef22
Adrian Covarrubias <145578758+adrife2005@users.noreply.github.com>
2025-02-10 16:47:29 -0600

    merge (#5)
    * first commint monorepo
    
    * Feature/SBO-2: Project Configuration(Ecosystems, Package).
    
    * production: added react + tailwindcss dependecies
    
    * production: added tailwindcss as pluging
    
    * production: removed asterisco and added zustand dependeci
    
    * production: added react-router axios dependecies
    
    * production: wrap application with browserrouterimport
    
    * production: added template for pr
    
    ---------
    
    Co-authored-by: sebah <sebascursoshtml@gmail.com>

---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className="text-2xl">Hola</h1>
+}
+
+export default App
---
+++ server/socialbotlabs-bot/src/main/java/com/socialbotlabs/serverbot/socialbotlabs_bot/SocialbotlabsBotApplication.java
@@ -0,0 +1,12 @@
+package com.socialbotlabs.serverbot.socialbotlabs_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialbotlabsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialbotlabsBotApplication.class, args);
+	}
+}
