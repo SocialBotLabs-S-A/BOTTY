706db48790aa591a7a6f531351cc0549740b0702
Heyker <heykervargas13@gmail.com>
2025-02-13 08:20:12 -0400

    fix: colocar el componente del formulario de register en la ruta que le corresponde.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import { Route } from "react-router";
 import RoutesWithNotFound from "./components/RoutesWithNotFound";
 import AuthGuard from "./guard/AuthGuard";
+import RegisterForm from "./pages/register/RegisterForm";
 
 function App() {
   return (
     <RoutesWithNotFound>
       <Route path="/login" element={<span>Login Page</span>} />
-      <Route path="/register" element={<span>Register Page</span>} />
+      <Route path="/register" element={<RegisterForm/>} />
       <Route element={<AuthGuard />}>
           {/* Aqui van a estar las rutas privadas */}
       </Route>
