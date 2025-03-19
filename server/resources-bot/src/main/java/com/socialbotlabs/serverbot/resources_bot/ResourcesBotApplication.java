c17540c90d2561e8d8eca90f2715d24665f87e12
Eduardo Maravilla <eduardomaravillag@gmail.com>
2025-03-19 15:36:55 -0600

    feature/sbo-25 create resource server

---
+++ server/resources-bot/src/main/java/com/socialbotlabs/serverbot/resources_bot/ResourcesBotApplication.java
@@ -0,0 +1,13 @@
+package com.socialbotlabs.serverbot.resources_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcesBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcesBotApplication.class, args);
+	}
+
+}
